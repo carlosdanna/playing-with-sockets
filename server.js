1c53574d29e90c254784813f3248c65172ccb629
Carlos Danna <carlos.danna@gmail.com>
2016-04-22 17:00:03 -0400

    refactoring client to work with angular instead of jquery

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require("socket.io")(http);
 
 var port = process.env.PORT || 3000;
 
 var history = [];
+
+app.use('/assets', express.static(__dirname + '/public'))
 
 app.get('/', function(req,res){
     res.sendFile(__dirname + "/index.html");
@@ -13,23 +16,18 @@ app.get('/', function(req,res){
 io.on('connection', function(socket){
     console.log("client connected");
     socket.on('get history', function(){
-        console.log('get history triggered', history);
         io.emit('get history', history);
     });
+
     socket.on('chat message', function(user){
         history.push(user);
         io.emit('chat message', user);
-
     });
+
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
-
-
 });
-
-
-
 
 http.listen(port, function(){
     console.log(`listening on port: ${port}`);
